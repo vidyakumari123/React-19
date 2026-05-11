1c82649738934149923073157131e0f63799f5a7
vidyakumari123 <127233800+vidyakumari123@users.noreply.github.com>
2026-05-11 19:03:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>React 19 Tutorials</h1>
+      <p>Check the branches in this repo for lesson wise code</p>
+    </div>
+  );
+}
+
+export default App;
